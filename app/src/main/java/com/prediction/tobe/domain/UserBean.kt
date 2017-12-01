e01066b4fd1a89ed4eebaee846cd2111d86b13b0
Yahor Fralou <yahor.fralou@datalex.com>
2017-12-01 14:50:51 +0300

    Plug in version updated Unused constructor removed

--- app/src/main/java/com/prediction/tobe/domain/UserBean.kt
+++ app/src/main/java/com/prediction/tobe/domain/UserBean.kt
@@ -1,7 +1,6 @@
 package com.prediction.tobe.domain
 
 data class UserBean(val id: String, val name: String?, val photoUrl: String?, val email: String?) {
-    constructor() : this("", null, null, null)
 
     override fun equals(other: Any?): Boolean = other is UserBean && other.id == id
 
